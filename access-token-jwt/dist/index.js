cee0ba938b807568949d61288d722f58dc154fe9
Simen H. Bakken <simen.bakken@panopticxr.no>
2022-07-21 14:38:23 +0200

    Removed accidentally commited files

--- access-token-jwt/dist/index.js
+++
@@ -1,4 +0,0 @@
-export { default as jwtVerifier, } from './jwt-verifier';
-export { InvalidTokenError, UnauthorizedError, InsufficientScopeError, } from 'oauth2-bearer';
-export { default as discover } from './discovery';
-export { claimCheck, claimEquals, claimIncludes, requiredScopes, } from './claim-check';
